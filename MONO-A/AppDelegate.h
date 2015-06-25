b64b626c3305f804ec715889f46bff8ff44a862b
DearJ <1770421128@qq.com>
2015-06-25 14:32:32 +0800

    Initial Commit

---
+++ MONO-A/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MONO-A
+//
+//  Created by lanouhn on 15/6/25.
+//  Copyright (c) 2015年 lanouhn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
